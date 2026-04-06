c499d4bf9d4ddacaac41beb94255a71a751eceaa
assiaKd <44897029+assiaKd@users.noreply.github.com>
2026-04-06 12:34:39 -0400

    update provider in tf file

--- Infrastructure/terraform.tf
+++ Infrastructure/terraform.tf
@@ -1,5 +1,12 @@
-provider "azurerm" {
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.67"
+    }
+  }
+
+  required_version = ">= 1.3"
 }
 
 variable "resource_group_name" {
